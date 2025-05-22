bf9041bb2ec4f9f24619928b388966bb98d79d1f
anarezmuves <anapetrusca2002@gmail.com>
2025-05-22 05:02:42 +0300

    Added the C file

---
+++ sampleFunction.c
@@ -0,0 +1,19 @@
+// Author: Anamaria Rezmuves
+
+#include <stdio.h>
+
+// Declaring the sum printing function
+
+int printSum(int number1, int number2) {
+  
+  printf("%d", number1 + number2);
+  return 0;
+}
+
+int main() {
+
+  // Print the sum between 8 and 3
+
+  printSum(8, 3);
+  return 0;
+}
